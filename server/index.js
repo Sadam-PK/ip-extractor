bd59fb2ac58b19692e12f7b906220b49fdc7aa96
sadam-PK <sadamccr@gmail.com>
2024-09-16 16:09:44 +0500

    cors fixed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 const ipInfoBaseUrl = process.env.IPINFO_BASE_URL;
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ip-extractor-mmb4.vercel.app",
+  })
+);
 
 // Middleware to capture user's IP address
 app.use(requestIp.mw());
